8003850b94733509f3f675ffe789ae0dfc04007f
vanuragmishra <vanuragmishra@gmail.com>
2026-08-28 17:41:01 +0530

    added vnet rg sa in preprod

---
+++ environment/Preprod/main.tf
@@ -0,0 +1,21 @@
+variable "rg_p" {}
+variable "sa_p" {}
+variable "vnet_p" {
+
+}
+
+module "rg_mb" {
+  source = "../../Module/AZ_Resource_Group"
+  rg_m   = var.rg_p
+}
+
+module "sa_mb" {
+  source = "../../Module/AZ_Storage_Account"
+  sa_m   = var.sa_p
+
+}
+
+module "vnet_mb" {
+  source = "../../Module/AZ_VNet"
+  vnet_m = var.vnet_p
+}
---
+++ environment/Preprod/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "0e4b359f-769c-4908-9180-91beab7e2ebf"
+}
